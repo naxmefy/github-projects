4dd904bd8d8ee560ada03f21b0e998c728f88a50
MRW Neundorf <ninevillage@gmail.com>
2021-09-26 09:38:22 +0200

    feat: added shared_preferences to DI

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final di = GetIt.I;
 
-Future initDI() async {}
+Future initDI() async {
+  di.registerLazySingletonAsync<SharedPreferences>(() async => SharedPreferences.getInstance());
+}
