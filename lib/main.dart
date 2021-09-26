22a0a57b824db23634ce2e3bdbcc3408a86cfe57
MRW Neundorf <ninevillage@gmail.com>
2021-09-26 09:01:56 +0200

    feat: load DI and set preferred orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:githubprojects/di/di.dart';
 import 'package:githubprojects/presentation/presentation.dart';
+import 'package:pedantic/pedantic.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  unawaited(SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]));
+  unawaited(initDI());
   runApp(const GHPApp());
 }
