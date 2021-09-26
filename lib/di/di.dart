9e82b7cafdd4ff6931d0b4ad4a8295c083bff52b
MRW Neundorf <ninevillage@gmail.com>
2021-09-26 10:16:59 +0200

    feat: extent DI
    - with FlutterSecureStorage
    - and FlutterAppAuth

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -6,4 +8,6 @@ final di = GetIt.I;
 Future initDI() async {
   di.registerLazySingletonAsync<SharedPreferences>(
       () async => SharedPreferences.getInstance());
+  di.registerLazySingleton<FlutterSecureStorage>(() => const FlutterSecureStorage());
+  di.registerLazySingleton<FlutterAppAuth>(() => FlutterAppAuth());
 }
